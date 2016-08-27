0ac7e401b78223d7bafb75147a52b8a20442acf5
Mindy Carson <mcarson1111@gmail.com>
2016-08-27 14:23:57 -0700

    did some styling

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,45 @@
 // This is where it all goes :)
+//= require jquery
+
+var User = {
+  name: 'User',
+  hardinessZone: '3',
+  soilType: 'loamy',
+  allVeggies: [ ],
+  chosenVeggies: [ ]
+  // plotSize
+}
+
+function sendSoilTypeAndClimate() {
+  //show about div and submit button
+
+  //get the user input (text fields)
+
+  //save user input to User object
+
+  //send unpout to API
+
+  //send results to next interaction (show veggies)
+
+  //hide about div and submit button when clicked
+}
+
+function showVeggiesToUser() {
+  //show veggies div and submit button
+
+  //loop through allVeggies array and display names in checkbox
+
+  //save chosen veggies to User object (chosenVeggies)
+
+  //hide veggies list div and submit button when button clicked
+}
+
+function showPlotAndVeggieList() {
+  //display div of veggies
+  
+  //display plot
+
+  //compare chosenVeggies array and allVeggies array to find attributes of veggies chosen to display
+
+  //display image of chosen veggies
+}
